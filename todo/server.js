ed8b64c4802216e2841bf48c5679c1db2b00dd5b
Rohan Tandel <67rohan@gmail.com>
2026-07-31 10:00:17 +0530

    todo app update on node

--- todo/server.js
+++ todo/server.js
@@ -15,6 +15,9 @@ app.post('/product', productCreate);
 
 app.post('/user/:userId', getUser);
 app.post('/users', getUsers);
+app.put('/user/:userId', updateUser);
+
+const pool = pgConnect();
 
 async function pgConnect()
 {
@@ -25,7 +28,7 @@ async function pgConnect()
         port: 5432,
         database: 'postgres',
         max:20,
-        _connectionTimeoutMillis: 3000,
+        connectionTimeoutMillis: 3000,
     });
     // console.log(await pool.query('SELECT NOW()'))
     //
@@ -52,7 +55,6 @@ async function userLogin(req, res)
 
 async function userRegister(req, res)
 {
-    const pool = await pgConnect();
     const { email, password } = req.body;
 
     if (!email || email.length < 1 || email.length > 255 || typeof email !== 'string') {
@@ -158,8 +160,6 @@ async function getUser(req, res)
         return res.status(404).json({ error: 'validation errors', message: 'user does not exist'});
     }
 
-    const pool = await pgConnect();
-
     const userQuery = `SELECT * FROM users WHERE user_id = ${userId}`;
 
     const result = await pool.query(userQuery);
@@ -174,11 +174,11 @@ async function getUser(req, res)
 
 async function getUsers(req, res)
 {
-    const pool = await pgConnect();
+    const email = req?.query?.email ?? null;
 
-    const userQuery = `SELECT * FROM users`;
+    const userQuery = `SELECT * FROM users where email = $1 OR NOT EXISTS(SELECT 1 FROM users WHERE email = $1)`;
 
-    const result = await pool.query(userQuery);
+    const result = await pool.query(userQuery, [email]);
 
     if(!result || !result.rows || !result.rows.length) {
         return res.status(404).json({ error: 'user does not exist'});
@@ -190,5 +190,47 @@ async function getUsers(req, res)
 
 }
 
+async function updateUser(req, res)
+{
+    let userId = req.params.userId;
+    const email = req.body.email;
+    const errors = [];
+    if (!email || typeof email !== 'string') {
+        errors.push('email is required and have at least 3 characters long');
+    }
+
+    if (userId) {
+        userId = parseInt(userId, 10);
+
+        if (userId < 0 || Number.isNaN(userId)) {
+            errors.push('userId is required and have at least 3 characters long');
+        }
+    } else {
+        errors.push('userId is required');
+    }
+
+    if (errors.length > 0) {
+        return res.status(200).json({message: "validation errors", errors: errors})
+    }
+
+    const query = `update users SET email = $1, username = $1 where user_id = $2 RETURNING user_id, firstname, lastname, username, email, created_at, updated_at;`;
+
+    try {
+
+        const result = await pool.query(query, [email, userId]);
+
+        console.log(result);
+        if (query.length > 0 && result.rowCount > 0) {
+            const {password, ...formattedResult} = result.rows[0];
+            return res.status(200).json({status: "success", data: formattedResult});
+        }
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.status(400).json({error: "user exists with that email address"});
+        }
+        return res.status(500).json({error: error.message});
+    }
+
+}
 
 app.listen(3000, () => console.log('Listening on port 3000'));
