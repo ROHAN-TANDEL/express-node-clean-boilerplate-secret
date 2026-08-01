eb00debb8fbe4dae42d5c9b048a51527993e853a
Rohan Tandel <67rohan@gmail.com>
2026-08-01 16:44:59 +0530

    PORT is not configured from dotnev

--- production-lab/backend-engineering-lab/projects/api-revised/src/server.ts
+++ production-lab/backend-engineering-lab/projects/api-revised/src/server.ts
@@ -1,23 +1,34 @@
 //new express
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const PORT = 3000;
+const config = {
+
+    port: Number(
+
+        process.env.PORT ?? 3000
+
+    )
+
+};
 
 // new route
 app.get("/health", (req, res) => {
 
     res.json({
 
-        status: "healthy V1"
+        status: "healthy V3"
 
     });
 
 });
 
 // server started
-app.listen(PORT, () => {
+app.listen(config.port, () => {
 
     console.log("Server Started");
 
