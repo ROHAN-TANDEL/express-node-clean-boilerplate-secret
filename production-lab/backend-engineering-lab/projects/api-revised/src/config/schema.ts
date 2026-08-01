69d4847866bf185a59321796f0bbccab792b2711
Rohan Tandel <67rohan@gmail.com>
2026-08-01 17:11:15 +0530

    additional validation added in here

--- production-lab/backend-engineering-lab/projects/api-revised/src/config/schema.ts
+++ production-lab/backend-engineering-lab/projects/api-revised/src/config/schema.ts
@@ -3,6 +3,18 @@ import z from "zod";
 
 export const environmentSchema =z.object({
 
-    PORT: z.coerce.number()
+    APP_NAME: z.string(),
+    PORT: z.coerce.number(),
+    NODE_ENV: z.enum([
+
+        "development",
+
+        "test",
+
+        "staging",
+
+        "production"
+
+    ])
 
 });
