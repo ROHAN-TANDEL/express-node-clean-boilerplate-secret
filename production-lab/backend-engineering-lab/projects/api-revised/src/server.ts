9d475a872697af422e8be251d551582deb689c6e
Rohan Tandel <67rohan@gmail.com>
2026-08-01 16:43:08 +0530

    PORT is seperated and is added in a constant

--- production-lab/backend-engineering-lab/projects/api-revised/src/server.ts
+++ production-lab/backend-engineering-lab/projects/api-revised/src/server.ts
@@ -3,19 +3,21 @@ import express from "express";
 
 const app = express();
 
+const PORT = 3000;
+
 // new route
 app.get("/health", (req, res) => {
 
     res.json({
 
-        status: "healthy"
+        status: "healthy V1"
 
     });
 
 });
 
 // server started
-app.listen(3000, () => {
+app.listen(PORT, () => {
 
     console.log("Server Started");
 
